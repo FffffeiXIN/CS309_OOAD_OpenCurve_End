ac9464d552508eab73c0dcee9f32dee91849d34b
LinJie <1249706509@qq.com>
2022-09-12 17:13:09 +0800

    实现StudentService

---
+++ service/service_education/src/main/java/com/sustech/service_education/service/impl/StudentEducationServiceImpl.java
@@ -0,0 +1,10 @@
+package com.sustech.service_education.service.impl;
+
+import com.sustech.service_education.service.StudentEducationService;
+
+public class StudentEducationServiceImpl implements StudentEducationService {
+    @Override
+    public boolean chooseCourse(int id) {
+        return false;
+    }
+}
