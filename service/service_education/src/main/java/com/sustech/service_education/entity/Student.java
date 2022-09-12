1e9408795d832a4905e74ffe848a92c9e811bcc6
dazhi0619 <dazhi0619@gmail.com>
2022-09-12 19:32:14 +0800

    add Student.java and corresponding StudentMapper.java and .xml

--- service/service_education/src/main/java/com/sustech/service_education/entity/Student.java
+++ service/service_education/src/main/java/com/sustech/service_education/entity/Student.java
@@ -4,5 +4,38 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(value = { "hibernateLazyInitializer", "handler" })
 public class Student implements User{
+    int id;
+    String password;
+    String name;
+    String department;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public boolean comparePassword(String password) {
+        return password.equals(this.password);
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
---
+++ service/service_education/src/main/java/com/sustech/service_education/mapper/StudentMapper.java
@@ -0,0 +1,9 @@
+package com.sustech.service_education.mapper;
+
+import com.sustech.service_education.entity.Student;
+
+import java.util.List;
+
+public interface StudentMapper {
+    List<Student> queryAll();
+}
