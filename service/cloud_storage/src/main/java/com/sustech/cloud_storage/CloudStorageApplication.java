2f8e949817d204455fa69342ced0bcf8339eb47f
LinJie <1249706509@qq.com>
2022-09-15 00:58:42 +0800

    添加静态资源上云的解决方案

--- service/cloud_storage/src/main/java/com/sustech/cloud_storage/CloudStorageApplication.java
+++ service/cloud_storage/src/main/java/com/sustech/cloud_storage/CloudStorageApplication.java
@@ -1,14 +1,13 @@
 package com.sustech.cloud_storage;
 
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.sustech"})
 public class CloudStorageApplication {
     public static void main(String[] args) {
