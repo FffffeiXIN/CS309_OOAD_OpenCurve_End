54a8dd961c247539dc8c43a49c602ed9334aecc3
LinJie <1249706509@qq.com>
2022-09-14 17:10:42 +0800

    解决跨域问题

--- service/service_education/src/main/java/com/sustech/service_education/controller/StudentEducationController.java
+++ service/service_education/src/main/java/com/sustech/service_education/controller/StudentEducationController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- service/service_education/src/main/java/com/sustech/service_education/entity/SuperManager.java
+++ service/service_education/src/main/java/com/sustech/service_education/entity/SuperManager.java
@@ -1,38 +1,4 @@
 package com.sustech.service_education.entity;
 
 public class SuperManager implements Manager{
-    @Override
-    public boolean addTeacher(User user) {
-        return false;
-    }
-
-    @Override
-    public boolean addStudent(User user) {
-        return false;
-    }
-
-    @Override
-    public boolean delete(User user) {
-        return false;
-    }
-
-    @Override
-    public boolean addCourse(Course course) {
-        return false;
-    }
-
-    @Override
-    public boolean delete(Course course) {
-        return false;
-    }
-
-    @Override
-    public boolean addDepartment(Department department) {
-        return false;
-    }
-
-    @Override
-    public boolean delete(Department department) {
-        return false;
-    }
 }
