461a412316df3559b52fc60049b00a75fc5143ed
LinJie <1249706509@qq.com>
2022-09-15 19:50:38 +0800

    为静态资源上云提供Swagger测试

--- service/cloud_storage/src/main/java/com/sustech/cloud_storage/CloudStorageApplication.java
+++ service/cloud_storage/src/main/java/com/sustech/cloud_storage/CloudStorageApplication.java
@@ -6,9 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = {"com.sustech"})
+@EnableSwagger2
+//@EnableWebMvc
 public class CloudStorageApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudStorageApplication.class,args);
--- service/cloud_storage/src/main/java/com/sustech/cloud_storage/controller/FileUploadingController.java
+++ service/cloud_storage/src/main/java/com/sustech/cloud_storage/controller/FileUploadingController.java
@@ -2,22 +2,26 @@ package com.sustech.cloud_storage.controller;
 
 import com.sustech.cloud_storage.service.FileUploadingService;
 import com.sustech.commonutils.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
 @CrossOrigin
 @RequestMapping("/cloud_storage/file")
+@Api(value = "接口测试类", tags = "云储存测试类")
 public class FileUploadingController {
 
     @Autowired
     private FileUploadingService fileUploadingService;
 
 
+    @ApiOperation(value = "上传文件至云端")
     @PostMapping("uploading")
     public Result upload(MultipartFile file){
         String url= fileUploadingService.upload(file);
