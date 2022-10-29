867f4847441fde4b0682682ace3b6101fa90b2e4
lsmxtc <12012939@mail.sustech.edu.cn>
2022-10-29 11:54:29 +0800

    完善了teacherLogin和managerLogin,加了相应mapper和抽象类

--- service/service_education/src/main/java/com/sustech/service_education/controller/LoginController.java
+++ service/service_education/src/main/java/com/sustech/service_education/controller/LoginController.java
@@ -7,6 +7,7 @@ import com.sustech.commonutils.enums.Code;
 import com.sustech.service_education.service.login.LoginService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = {"接口测试"})
 public class LoginController {
     @GetMapping("user")
-    @ApiOperation(value = "学生登录模块测试")
-    public Result studentLogin(String userid,String content,String loginType){
+    @ApiOperation(value = "登录模块测试")
+    public Result login(String userid,String content,String loginType){
         LoginService loginService;
         if(LoginService.loginService.containsKey(loginType)){
             loginService=LoginService.loginService.get(loginType);
