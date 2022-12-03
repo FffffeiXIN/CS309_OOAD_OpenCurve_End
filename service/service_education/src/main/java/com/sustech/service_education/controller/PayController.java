d92cd52db87fdf649fd8498eb2c408aaba5f01b9
lsmxtc <12012939@mail.sustech.edu.cn>
2022-12-03 15:03:42 +0800

    h

--- service/service_education/src/main/java/com/sustech/service_education/controller/PayController.java
+++ service/service_education/src/main/java/com/sustech/service_education/controller/PayController.java
@@ -5,6 +5,7 @@ import com.alipay.api.AlipayApiException;
 import com.sustech.commonutils.Result;
 import com.sustech.service_education.service.pay.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,19 +18,21 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/education/pay")
+@CrossOrigin
 public class PayController {
 
     @Autowired
     private PayService payService;
 
     @GetMapping("/purchase")
-    public Result pay(String courseId,String userId, HttpServletRequest request, HttpServletResponse response) throws AlipayApiException, IOException {
+    public void pay(String courseId,String userId, HttpServletRequest request, HttpServletResponse response) throws AlipayApiException, IOException {
         Result result = payService.createOrder(courseId,userId);
         String orderId = (String) result.getData().get("id");
         String res_frame = payService.purchase(orderId, request, response);
-        Map<String,Object> data=new HashMap<>();
-        data.put("order",orderId);
-        data.put("frame", res_frame);
-        return Result.ok().code(200).message("支付成功!").data(data);
+//        return res_frame;
+//        Map<String,Object> data=new HashMap<>();
+//        data.put("order",orderId);
+//        data.put("frame", res_frame);
+//        return Result.ok().code(200).message("支付成功!").data(data);
     }
 }
