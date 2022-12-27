2c6d06812d22ba97c8109b3aace150a21b19b722
lsmxtc <12012939@mail.sustech.edu.cn>
2022-12-28 01:14:40 +0800

    简化login

--- service/service_education/src/main/java/com/sustech/service_education/service/login/LoginService.java
+++ service/service_education/src/main/java/com/sustech/service_education/service/login/LoginService.java
@@ -1,9 +1,6 @@
 package com.sustech.service_education.service.login;
-
-
 import com.sustech.commonutils.Result;
 import org.springframework.stereotype.Service;
-
 import javax.mail.MessagingException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -13,7 +10,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public interface LoginService {
-    Map<String, LoginService> loginService = new ConcurrentHashMap<>();
-    Result login(String username,String content) throws UnknownHostException, MessagingException;
+//    Map<String, LoginService> loginService = new ConcurrentHashMap<>();
+    Result studentLogin(String username,String content, String type);
+    Result teacherLogin(String username,String content, String type);
+    Result managerLogin(String username,String content, String type);
 
 }
---
+++ service/service_education/src/main/java/com/sustech/service_education/service/login/impl/PasswordLoginServiceImpl.java
@@ -0,0 +1,17 @@
+//package com.sustech.service_education.service.login.impl;
+//
+//import com.sustech.commonutils.Result;
+//import com.sustech.commonutils.enums.Code;
+//import com.sustech.service_education.entity.Student;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class PasswordLoginServiceImpl extends  {
+//    @Override
+//    Result doLogin(Student student, String content) {
+//        if(student.comparePassword(content)){
+//            return Result.ok().code(200).message("学生登录成功");
+//        }
+//        else return Result.error().code(Code.LOGIN_ERROR.getCode()).message("密码错误");
+//    }
+//}
