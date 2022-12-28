7e067c5676c3c0ca1085a46f2165e854d66aab60
LinJie <1249706509@qq.com>
2022-12-28 19:03:57 +0800

    assignment Model

--- service/cloud_storage/src/main/java/com/sustech/cloud_storage/CloudStorageApplication.java
+++ service/cloud_storage/src/main/java/com/sustech/cloud_storage/CloudStorageApplication.java
@@ -4,11 +4,13 @@ package com.sustech.cloud_storage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableSwagger2
 //@EnableWebMvc
