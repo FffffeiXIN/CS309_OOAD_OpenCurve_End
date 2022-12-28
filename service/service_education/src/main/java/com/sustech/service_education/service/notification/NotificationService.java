89ddcc8b2ac1f7397580a7208e741c26d224f8dc
xavier <xshaw@gmail.com>
2022-12-28 12:57:24 +0800

    12.28 update notification interface

--- service/service_education/src/main/java/com/sustech/service_education/service/notification/NotificationService.java
+++ service/service_education/src/main/java/com/sustech/service_education/service/notification/NotificationService.java
@@ -5,8 +5,7 @@ import com.sustech.commonutils.Result;
 public interface NotificationService {
     Result addNotification(String title, String course_id, String teacher_id, String modified_date, String content);
 
-//    Result getNotificationsByCourse(String course_id);
-
+    Result getNotificationsByCourse(String course_id);
     Result getNotificationByTeacher(String teacher_id);
     Result getNotificationByManager(String manager_id);
     Result getNotificationByStudent(String student_id);
