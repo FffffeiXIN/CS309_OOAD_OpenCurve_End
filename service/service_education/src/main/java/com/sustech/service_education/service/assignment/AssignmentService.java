9a688ca716e82da177f120f45696e19749f27ced
xavier <xshaw@gmail.com>
2022-12-30 14:11:48 +0800

    12.30 update assignment score

--- service/service_education/src/main/java/com/sustech/service_education/service/assignment/AssignmentService.java
+++ service/service_education/src/main/java/com/sustech/service_education/service/assignment/AssignmentService.java
@@ -13,25 +13,26 @@ import java.util.List;
 public class AssignmentService {
     @Autowired
     AssignmentMapper assignmentMapper;
-    public Result addAssignment(int id,String title,String course_id,String teacher_id,String due_date,int resubmission_allowed,String accept_resubmission_until,String requirements){
-        assignmentMapper.addAssignment(id,title,course_id,teacher_id,due_date,resubmission_allowed,accept_resubmission_until,requirements);
+
+    public Result addAssignment(int id, String title, String course_id, String teacher_id, String due_date, int resubmission_allowed, String accept_resubmission_until, String requirements) {
+        assignmentMapper.addAssignment(id, title, course_id, teacher_id, due_date, resubmission_allowed, accept_resubmission_until, requirements);
         return Result.ok().code(200);
     }
 
-    public Result submitAssignment(int assignment_id,String student_id,String text, List<String> list){
-        StringBuffer stringBuffer=new StringBuffer();
-        list.forEach(x->stringBuffer.append(x).append(","));
-        if(assignmentMapper.selectSubmissionById(assignment_id,student_id).size()==0){
-            assignmentMapper.addStudentSubmission(assignment_id,student_id,stringBuffer.toString(),text);
+    public Result submitAssignment(int assignment_id, String student_id, String text, List<String> list) {
+        StringBuffer stringBuffer = new StringBuffer();
+        list.forEach(x -> stringBuffer.append(x).append(","));
+        if (assignmentMapper.selectSubmissionById(assignment_id, student_id).size() == 0) {
+            assignmentMapper.addStudentSubmission(assignment_id, student_id, stringBuffer.toString(), text);
             return Result.ok().code(200).message("新建提交成功");
         }
-        assignmentMapper.updateStudentSubmission(assignment_id,student_id,stringBuffer.toString(),text);
+        assignmentMapper.updateStudentSubmission(assignment_id, student_id, stringBuffer.toString(), text);
         return Result.ok().code(200).message("更新提交成功");
     }
 
-    public Result getSubmission(int assignment_id,String student_id){
-        HashMap<String,Object> map = assignmentMapper.selectSubmissionById(assignment_id,student_id);
-        if(map==null||map.size()==0) throw new SourceNotFoundException();
+    public Result getSubmission(int assignment_id, String student_id) {
+        HashMap<String, Object> map = assignmentMapper.selectSubmissionById(assignment_id, student_id);
+        if (map == null || map.size() == 0) throw new SourceNotFoundException();
         return Result.ok().code(200).data(map);
     }
 }
