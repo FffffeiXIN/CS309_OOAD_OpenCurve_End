1a28e5ea85757edab6e3dc9df913b26426e97743
lsmxtc <12012939@mail.sustech.edu.cn>
2023-01-03 14:55:41 +0800

    支付

--- service/service_education/src/main/java/com/sustech/service_education/controller/PayController.java
+++ service/service_education/src/main/java/com/sustech/service_education/controller/PayController.java
@@ -3,12 +3,10 @@ package com.sustech.service_education.controller;
 
 import com.alipay.api.AlipayApiException;
 import com.sustech.commonutils.Result;
+import com.sustech.service_education.service.course.CourseService;
 import com.sustech.service_education.service.pay.PayService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +22,9 @@ public class PayController {
     @Autowired
     private PayService payService;
 
+    @Autowired
+    private CourseService courseService;
+
     @GetMapping("/purchase")
     public void pay(String courseId,String userId, HttpServletRequest request, HttpServletResponse response) throws AlipayApiException, IOException {
         Result result = payService.createOrder(courseId,userId);
@@ -34,5 +35,13 @@ public class PayController {
 //        data.put("order",orderId);
 //        data.put("frame", res_frame);
 //        return Result.ok().code(200).message("支付成功!").data(data);
+    }
+
+    @PostMapping("/callback")
+    public void callback(String out_trade_no){
+        payService.paySuccess(out_trade_no);
+        String course_id = out_trade_no.split("/$")[0];
+        String student_id = out_trade_no.split("/$")[1];
+        courseService.ad
     }
 }
