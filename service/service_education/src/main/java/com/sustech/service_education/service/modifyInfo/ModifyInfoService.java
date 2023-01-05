e010a1376ba4feb980d5dc0bf8f626840e3431ae
lsmxtc <12012939@mail.sustech.edu.cn>
2023-01-05 23:55:28 +0800

    info

--- service/service_education/src/main/java/com/sustech/service_education/service/modifyInfo/ModifyInfoService.java
+++ service/service_education/src/main/java/com/sustech/service_education/service/modifyInfo/ModifyInfoService.java
@@ -2,7 +2,11 @@ package com.sustech.service_education.service.modifyInfo;
 
 import com.sustech.commonutils.Result;
 import com.sustech.service_education.entity.Student;
+import com.sustech.service_education.entity.SuperManager;
+import com.sustech.service_education.entity.Teacher;
 import com.sustech.service_education.mapper.StudentMapper;
+import com.sustech.service_education.mapper.SuperManagerMapper;
+import com.sustech.service_education.mapper.TeacherMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +18,30 @@ import java.util.Objects;
 public class ModifyInfoService {
     @Autowired
     StudentMapper studentMapper;
+
+    @Autowired
+    TeacherMapper teacherMapper;
+    @Autowired
+    SuperManagerMapper superManagerMapper;
     public Result updateInfo(String id, String name, String department, String college, String grade, String picture_url, String birthday, String gender, String region, String address) {
         studentMapper.update(id, name, department, college, grade, picture_url, birthday, gender,region,address);
         return Result.ok().code(200).message("修改信息成功");
     }
 
-    public Result getInfo(String id){
-        Student res = studentMapper.selectStudentById(id);
+    public Result getInfo(String id, String userType){
         Map<String, Object> map = new HashMap<>();
-        map.put("Info",res);
+        if (userType.equals("Student")){
+            Student res = studentMapper.selectStudentById(id);
+            map.put("Info",res);
+        }
+        else if (userType.equals("Teacher")){
+            Teacher res = teacherMapper.selectTeacherByEmail(id);
+            map.put("Info",res);
+        }
+        else {
+            SuperManager res = superManagerMapper.selectManagerById(id);
+            map.put("Info",res);
+        }
         return Result.ok().message("获取信息成功").data(map);
     }
 }
